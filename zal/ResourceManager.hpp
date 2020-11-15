f47ffcf43a4ed346399318e8a5b5e40b64d52edf
miloszwnuk <01157070@pw.edu.pl>
2020-11-15 21:38:39 +0000

    done

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -2,7 +2,58 @@
 
 #include "Resource.hpp"
 
-class ResourceManager
+ class ResourceManager
 {
-    // Twoja implementacja tutaj
+    Resource* res;
+
+public:
+
+    ResourceManager()
+    {
+        res = new Resource;
+    }
+
+    ResourceManager(const ResourceManager& rm) // konstruktor kopiujacy
+    {
+        res = new Resource{ *rm.res };
+    }
+
+    ResourceManager(ResourceManager&& rm) // 
+    {
+        res = nullptr;
+        res = rm.res;
+        rm.res = nullptr;
+    }
+
+    ~ResourceManager()
+    {
+        delete res;
+    }
+
+    double get()
+    {
+        return res->get();
+    }
+
+
+    ResourceManager operator=(ResourceManager&& rema)
+    {
+        if (&rema != this) {
+            delete res;
+            res = rema.res;
+            rema.res = nullptr;
+        }
+        return *this;
+    }
+
+    ResourceManager operator=(const ResourceManager& rema) //operator porownana
+    {
+        if (&rema != this) {
+            delete res;
+            res = new Resource{ *rema.res };
+        }
+        return *this;
+    }
+
+    
 };
